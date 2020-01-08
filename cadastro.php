ad70b8987ac783a758c205b3d75496f45af707c8
joaoclopes <joaoclopes14@gmail.com>
2020-01-08 17:01:19 -0300

    cadastro finalizado

--- cadastro.php
+++ cadastro.php
@@ -22,15 +22,18 @@ $u = new User
 </div>
 
 <?php
-isset($_POST['user'])
-{
+if(isset($_POST['user'])) {
     $user = addslashes($_POST['user']);
     $password = addslashes($_POST['password']);
     $name = addslashes($_POST['name']);
     if(!empty($name) && !empty($password) && !empty($name)){
         $u->connect("logindobanco","localhost","root","");
             if($u->msgError == ""){
-                $u->cadastrar($name,$user,(md5($password));
+                if($u->register($name,$user,(md5($password)){
+                    echo "cadastrado com sucesso";
+                } else {
+                    echo "Ja cadastrado";
+                }
             } else {
                 echo "Erro: ".$u->msgError;
             }
